d0a0aa75f7b1509ab1cf1b3a24f4808ee534f5eb
Minjun Gil <steeze1213@naver.com>
2023-02-14 15:17:16 +0900

    first commit

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,12 @@
+
+public class Letter {
+ 
+    public static void main(String[] args) {
+        String name = "steezer";
+        System.out.println("Hello, "+name+" ... "+name+" ... steeze1213 ... bye");
+         
+        double VAT = 10.0;
+        System.out.println(VAT);
+    }
+ 
+}
